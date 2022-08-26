96d4e3e0a799bf7fc1a03d1e1cff3824b4fea2a3
goriratatsu <tatsuhiko.shigoto@gmail.com>
2022-08-27 00:58:36 +0900

    update

--- rewrite.py
+++ rewrite.py
@@ -6,6 +6,8 @@ import re
 file_path = ''
 file_author = ''
 file_join = []
+name = []
+make_test_data = False
 
 os.system(f'mkdir {file_author}')
 
@@ -14,30 +16,58 @@ for i,j in enumerate(glob.glob(file_path + '/*/*/*')):
 
 os.system(f'nkf -w --overwrite {file_author}/*')
 
-for i in glob.glob(f'{file_author}/*'):
-    with open(i) as f:
-        s = f.read()
-        s = re.sub('《.*?》', '', s)
-        s = re.sub('［.*?］', '', s)
-        s = re.sub('｜', '', s)
-        s = re.sub('[「-」]', '', s)
-        s = s.split()
+def preprocessing():
+    for i in glob.glob(f'{file_author}/*'):
+        with open(i) as f:
+            s = f.read()
+            s = re.sub('《.*?》', '', s)
+            s = re.sub('［.*?］', '', s)
+            s = re.sub('｜', '', s)
+            s = re.sub('[「-」]', '', s)
+            s = s.replace('。', '')
+            s = s.replace('、', '')
+            s = s.split()
 
-        for i in range(2):
+            name.append(s[0])
+
+            for i in range(2):
+                try:
+                    del s[0:s.index('-------------------------------------------------------')+1]
+                except ValueError:
+                    break
+
             try:
-                del s[0:s.index('-------------------------------------------------------')+1]
-            except ValueError:
-                break
+                del s[s.index([s for s in s if '底本' in s][0]):]
+            except IndexError:
+                pass
 
-        try:
-            del s[s.index([s for s in s if '底本' in s][0]):]
-        except IndexError:
-            pass
+            s = ''.join(s)
+            file_join.append(s)
+    
 
-        s = ''.join(s)
-        file_join.append(s)
+if not make_test_data:
+    preprocessing()
 
-with open(f'{file_author}_texts.txt','w') as f:
-    f.write(''.join(file_join))
+    with open(f'{file_author}_train.txt','w') as f:
+        f.write(''.join(file_join))
 
-os.system(f'rm -r {file_author}')
+    os.system(f'mecab -Owakati {file_author}_train.txt -o {file_author}_train_wakati.txt')
+    os.system(f'nkf -w --overwrite {file_author}_train_wakati.txt')
+
+    os.system(f'rm {file_author}_train.txt')
+    os.system(f'rm -r {file_author}')
+
+
+else:
+    preprocessing()
+
+    for i,j in enumerate(file_join):
+        with open(f'{file_author}/bin_{name[i]}_{i}.txt', 'w') as q:
+            q.write(j)
+
+        os.system(f'mecab -Owakati {file_author}/bin_{name[i]}_{i}.txt -o {file_author}/{name[i]}_{i}_wakati.txt')
+
+    os.system(f'rm {file_author}/[0-9]*')
+    os.system(f'rm {file_author}/bin*')
+
+    os.system(f'nkf -w --overwrite {file_author}/*')
